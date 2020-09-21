d6a5223583e253f57d479061d035426fe340b1c4
Julian <lopezvignajulian@gmail.com>
2020-09-21 08:51:27 -0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import LayaoutBase from './layouts/LayaoutBase';
-import { ThemeProvider } from '@material-ui/styles';
-import theme from './theme';
-import Dashboard from './containers/Dashboard';
+import React from "react";
+import LayaoutBase from "./layouts/LayaoutBase";
+import { ThemeProvider } from "@material-ui/styles";
+import theme from "./theme";
+import Dashboard from "./containers/Dashboard";
 
-const App = () => {
-    return (
-        <ThemeProvider theme={theme}>
-            <LayaoutBase>
-                <Dashboard />
-            </LayaoutBase>
-        </ThemeProvider>
-    );
+const AppMemo = () => {
+  return (
+    <ThemeProvider theme={theme}>
+      <LayaoutBase>
+        <Dashboard />
+      </LayaoutBase>
+    </ThemeProvider>
+  );
 };
 
-export default App;
+export default AppMemo;
